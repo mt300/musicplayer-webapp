dca0f959381b6db1ab6efa2d54df2df0d8cc2281
mt300 <matheus.tomazi5@gmail.com>
2023-01-13 15:31:16 -0300

    almost finish

--- media/data.js
+++ media/data.js
@@ -2,9 +2,42 @@ var data = [
     "you can't control my heart (ft shiloh).mp3",
     "children-nick-cave.mpga","ciclo-sem-fim.mpga",
     "vou-vencer.mpga",
-    "superman-theme.mpga"
+    "superman-theme.mpga",
+    "you can't control my heart (ft shiloh).mp3",
+    "children-nick-cave.mpga","ciclo-sem-fim.mpga",
+    "vou-vencer.mpga",
+    "superman-theme.mpga",
+    "dsad3.mpga",
+    "children-nick-cave.mpga","ciclo-sem-fim.mpga",
+    "vou-vencer.mpga",
+    "Tentando.mpga",
+    "you can't control my heart (ft shiloh).mp3",
+    "children-nick-cave.mpga","ciclo-sem-fim.mpga",
+    "vou-vencer.mpga",
+    "superman-theme.mpga",
+    "you can't control my heart (ft shiloh).mp3",
+    "lastpage.mpga",
+    "ciclo-sem-fim.mpga",
+    "vou-vencer.mpga",
+    "you can't control my heart (ft shiloh).mp3",
+    "children-nick-cave.mpga","ciclo-sem-fim.mpga",
+    "vou-vencer.mpga",
+    "superman-theme.mpga",
+    "dsad3.mpga",
+    "children-nick-cave.mpga","ciclo-sem-fim.mpga",
+    "vou-vencer.mpga",
+    "Tentando.mpga",
+    "you can't control my heart (ft shiloh).mp3",
+    "children-nick-cave.mpga","ciclo-sem-fim.mpga",
+    "vou-vencer.mpga",
+    "superman-theme.mpga",
+    "you can't control my heart (ft shiloh).mp3",
+    "lastpage.mpga",
+    "ciclo-sem-fim.mpga",
+    "vou-vencer.mpga"
 ];
 
+var page = 1;
 // console.log(data)
 
 document.addEventListener("keydown", (e) => {
@@ -32,12 +65,19 @@ document.getElementById("searchInput").addEventListener("input", (e) => {
     searchChange(e.target.value);
 })
 
+
 const volume = function(operation) {
     var player = document.getElementById("player")
     if( operation == '+'){
         player.volume += 0.1
     }else if(operation == '-'){
         player.volume -= 0.1
+    }else if( operation == "x"){
+        if(player.volume == 0){
+            player.volume = 0.5
+        }else {
+            player.volume = 0
+        }
     }
 }
 const setSource = function () {
@@ -48,6 +88,11 @@ const setSource = function () {
     playPause() 
     source.src = "./media/" + data[currentTrack];
     player.load()
+
+    // set track tittle
+    var title = document.getElementById("track-title");
+    title.textContent = data[currentTrack].split('.')[0]
+    // duration and highlight
     var duration
     player.ondurationchange =  (e) => {
         
@@ -92,26 +137,46 @@ const clickItem = function (e) {
     setSource()
 }
 const volumeChange = function(e){
-    console.log(e.target.volume)
+    console.log(e.target.volume);
+    fetchVolume();
 }
 
+const fetchVolume = function () {
+    var player = document.getElementById("player");
+    var volume = player.volume
+    var bar = document.querySelector(".volumescope").style;
+    bar.setProperty("--vol-percentage",volume * 100 + "%");
+    var dot = document.getElementsByClassName("volumescope-dot")[0];
+    dot.style.left = volume*100 + "%";
+    var volumeIcon = document.querySelector("._volume");
+
+    console.log(volumeIcon)
+    // volumeIcon.setProperty("background-image","")
+}
 const fetchPlaylist = function () {
     // playlist element
     var playlist = document.getElementById("playlist")
     var child = playlist.querySelector(".playlist-item");
     playlist.removeChild(child)
+    var pagination = document.getElementById("pagination");
+    playlist.removeChild(pagination)
     
     // player element
     var player = document.getElementById("player")
     player.addEventListener("volumechange", volumeChange)
 
-
+    var itemCount = 0
     data.forEach( item => {
         // playlist display
         let song = document.createElement("div");
         song.classList.add("row", "playlist-item");
         song.innerText = item.split(".")[0]
-
+        if(itemCount > 9){
+            song.style.display = "none";
+        }else {
+            song.style.display = "block";
+        }
+        itemCount++;
         // description
         let description = document.createElement("div");
         description.classList.add("description");
@@ -129,6 +194,63 @@ const fetchPlaylist = function () {
         playlist.appendChild(song)
         
     });
+
+    // pagination
+    playlist.appendChild(pagination)
+    var paginationItem = pagination.querySelector(".number-item");
+    var listItems = document.getElementsByClassName("page-item")
+    // pagination.removeChild(listItems[1])
+    var lastChild = pagination.removeChild(listItems[2])
+    
+    for ( let i=1; i <= (data.length/10)+1; i++){
+        if( i != 1){
+            var newItem = document.createElement("li");
+            newItem.innerHTML = paginationItem.innerHTML
+            newItem.classList.add("page-item", "number-item")
+            let link = newItem.querySelector('a');
+            link.href="#";
+            link.innerText=i;
+            link.addEventListener("click", (e) => {
+                var list = document.getElementsByClassName("playlist-item");
+                page = e.target.innerHTML;
+                let max = Number(e.target.innerText)*10;
+                let min = max-10;
+                console.log(list.length)
+                for ( let i=0; i < list.length; i++){
+                    if( i < min || i >= max){
+                        list[i].style.display = "none"
+                        console.log("hide")
+                    }else {
+                        list[i].style.display = "block"
+                        console.log("show")
+                    }
+                }
+            })                    
+            console.log(newItem)
+            pagination.appendChild(newItem)
+        }else {
+            paginationItem.querySelector("a").addEventListener("click", (e) => {
+                var list = document.getElementsByClassName("playlist-item");
+                let max = Number(e.target.innerText)*10;
+                let min = max-10;
+                console.log(list.length)
+                for ( let i=0; i < list.length; i++){
+                    if( i < min || i >= max){
+                        list[i].style.display = "none"
+                        // console.log("hide")
+                    }else {
+                        list[i].style.display = "block"
+                        // console.log("show")
+                    }
+                }
+            })                   
+        }
+        // console.log("oi")
+    }
+    pagination.appendChild(lastChild)
+    // console.log(data.length)
+    
+
     // source playlist
     let source = document.createElement("source");
     source.src = "./media/" + data[0];
@@ -191,13 +313,66 @@ const previousTrack = function () {
 
 const searchChange = function (value) {
     var list = document.getElementsByClassName("playlist-item");
-    for (let i=0; i< (list.length * 2); i++) {
-        // console.log(list[i].innerText)
-        if(list[i].innerText.includes(value) == false && value != "" && value != " " && value != undefined){
-            list[i].style = "display: none;"
-        }else{
-            list[i].style = "display: block;"
-        }       
+    if(value != "" && value != undefined){
+
+        for (let i=0; i< (list.length * 2); i++) {
+            // console.log(list[i].innerText)
+            if(list[i].innerText.toLowerCase().includes(value.toLowerCase()) == false && value != " "){
+                list[i].style = "display: none;"
+            }else{
+                list[i].style = "display: block;"
+            }       
+        }
+    }else {
+        for ( let i=0; i < list.length; i++){
+            if( i < (page * 10) - 10 || i >= page * 10){
+                list[i].style.display = "none"
+                // console.log("hide")
+            }else {
+                list[i].style.display = "block"
+                // console.log("show")
+            }
+        }
     }
     // (value)
+}
+
+const nextPage = function() {
+    var list = document.getElementsByClassName("playlist-item")
+    // let max = Number(e.target.innerText)*10;   
+    // let min = max-10;            
+    if(page * 10 < list.length){
+        page++;
+        console.log(list.length)
+        for ( let i=0; i < list.length; i++){
+            if( i < (page * 10) - 10 || i >= page * 10){
+                list[i].style.display = "none"
+                // console.log("hide")
+            }else {
+                list[i].style.display = "block"
+                // console.log("show")
+            }
+        }
+
+    }
+}
+
+const previousPage = function() {
+    var list = document.getElementsByClassName("playlist-item")
+    // let max = Number(e.target.innerText)*10;   
+    // let min = max-10;            
+    if(page  > 1){
+        page--;
+        console.log(list.length)
+        for ( let i=0; i < list.length; i++){
+            if( i < (page * 10) - 10 || i >= page * 10){
+                list[i].style.display = "none"
+                // console.log("hide")
+            }else {
+                list[i].style.display = "block"
+                // console.log("show")
+            }
+        }
+
+    }
 }
